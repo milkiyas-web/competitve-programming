52ed7ae2e156930da7a5fae6cc2fefd4367b2b1a
Milkiyas <64133195+milkiyas-web@users.noreply.github.com>
2025-04-25 15:40:56 +0300

    Created 'Cyclic Components' by A2SV Hub

---
+++ 25-Apr-2025/Cyclic Components 342201.py
@@ -0,0 +1,126 @@
+# Problem: Cyclic Components - https://codeforces.com/problemset/problem/977/E
+
+import sys
+import collections
+input = sys.stdin.readline
+
+def inp():
+    return int(input())
+def inlt():
+    return list(map(int, input().split()))
+def insr():
+    return list(input().strip())
+def invr():
+    return map(int, input().split())
+
+# Defaultdict for handling missing keys automatically
+def dd_int():
+    return collections.defaultdict(int)
+def dd_list():
+    return collections.defaultdict(list)
+def dd_set():
+    return collections.defaultdict(set)
+
+# Linked List Implementation
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        new_node = ListNode(val)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def prepend(self, val):
+        new_node = ListNode(val, self.head)
+        self.head = new_node
+
+    def delete(self, val):
+        if not self.head:
+            return
+        if self.head.val == val:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next and current.next.val != val:
+            current = current.next
+        if current.next:
+            current.next = current.next.next
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.val, end=' -> ')
+            current = current.next
+        print('None')
+
+def is_in_bounds(grid, i, j):
+   return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+from types import GeneratorType
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+def solve():
+    n, m = invr()
+    adj = dd_list()
+    for i in range(m):
+        u,v = invr()
+        adj[u].append(v)
+        adj[v].append(u)
+    visited = [False] * (n+1)
+
+    @bootstrap
+    def dfs(node):
+        if not visited[node]:
+            visited[node] = True
+            for ne in adj[node]:
+                yield dfs(ne)
+        
+        yield
+
+
+    count = 0
+    for i in range(1, n+1):
+        if len(adj[i]) != 2:
+            dfs(i)
+    for i in range(1, n+1):
+        if not visited[i]:
+            count+=1
+            dfs(i)
+    print(count)
+
+
+
+def main():
+    multi = False  # If multiple test cases, set to True
+    t = int(input()) if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
