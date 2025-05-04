2115d2e04cf97f6cd95e46ab1bec883dc99f54b4
Milkiyas <64133195+milkiyas-web@users.noreply.github.com>
2025-05-04 19:27:33 +0300

    Created 'B - Empress Taytu and The Water Source' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 04-May-2025/B - Empress Taytu and The Water Source 348102.py
@@ -0,0 +1,105 @@
+# Problem: B - Empress Taytu and The Water Source - https://codeforces.com/gym/601269/problem/B
+
+import sys
+import collections
+input = sys.stdin.readline
+import math
+
+def inp():
+    return int(input())
+def inlt():
+    return list(map(int, input().split()))
+def insr():
+    return list(input().strip())
+def invr():
+    return map(int, input().split())
+
+# Defaultdict for handling missing keys automatically
+def dd_int():
+    return collections.defaultdict(int)
+def dd_list():
+    return collections.defaultdict(list)
+def dd_set():
+    return collections.defaultdict(set)
+
+# Linked List Implementation
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        new_node = ListNode(val)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def prepend(self, val):
+        new_node = ListNode(val, self.head)
+        self.head = new_node
+
+    def delete(self, val):
+        if not self.head:
+            return
+        if self.head.val == val:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next and current.next.val != val:
+            current = current.next
+        if current.next:
+            current.next = current.next.next
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.val, end=' -> ')
+            current = current.next
+        print('None')
+
+def is_in_bounds(grid, i, j):
+   return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+def solve():
+    n, k = invr()
+    d = inlt()
+    a = inlt()
+    
+    l, h = 1, max(d)
+    def is_valid(mid):
+        tk = 0
+        for i in range(n):
+            et = (d[i] + mid - 1)//mid
+            tk += a[i] * et  
+            if tk > k:
+                return False
+        return True
+    ans = -1
+    while l <= h:
+        mid = (l+h) // 2
+        if is_valid(mid):
+            ans = mid
+            h = mid - 1
+        else:
+            l = mid+1
+    print(ans)
+
+    
+        
+
+def main():
+    multi = True  # If multiple test cases, set to True
+    t = int(input()) if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
