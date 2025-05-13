ca8ff77e3539aeb5b1dea2680a1dc804750c7b42
Milkiyas <64133195+milkiyas-web@users.noreply.github.com>
2025-05-13 19:53:01 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 13-May-2025/Missing Number 354591.py
@@ -0,0 +1,26 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # nums.sort()
+        # l, r = 0, len(nums)
+
+        # while l < r:
+        #     mid = (l+r) // 2
+        #     if nums[mid] == mid:
+        #         l = mid+1
+        #     else:
+        #         r = mid
+        # return l
+        n = len(nums)
+        t = 0 
+        for n in nums:
+            t ^= n
+            print(t)
+        for i in range(len(nums)+1):
+            t^=i 
+        return t
+            
+
+
+        
