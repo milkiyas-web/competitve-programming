036457970b82aeda154718440568ad71d5b6e8c1
Milkiyas <64133195+milkiyas-web@users.noreply.github.com>
2025-05-14 10:40:26 +0300

    Created 'Hamming Distance' by A2SV Hub

---
+++ 14-May-2025/Hamming Distance 355061.py
@@ -0,0 +1,8 @@
+# Problem: Hamming Distance - https://leetcode.com/problems/hamming-distance/
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        ans = (x^y).bit_count()
+        return ans
+        
+        
