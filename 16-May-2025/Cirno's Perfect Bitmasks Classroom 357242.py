a10ce3f67e3a785da2a649a9335871497df07311
Milkiyas <64133195+milkiyas-web@users.noreply.github.com>
2025-05-16 22:05:21 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 16-May-2025/Cirno's Perfect Bitmasks Classroom 357242.py
@@ -0,0 +1,39 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+import sys, collections, math, random, bisect
+
+input = lambda: sys.stdin.readline().strip()
+
+inp = lambda: int(input())
+inlt = lambda: list(map(int, input().split()))
+insr = lambda: list(input())
+invr = lambda: map(int, input().split())
+
+def dd(type_func=int):
+    return collections.defaultdict(type_func)
+
+def is_in_bounds(grid, i, j):
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+directions = [(-1,0),(0,1),(0,-1),(1,0),(1,1),(-1,-1),(-1,1),(1,-1)]
+
+rand_no = random.randint(1, 1000000000)
+
+def solve():
+    x = inp()
+    if x == 1:
+        print(3)
+    elif (x & (x - 1)) == 0:
+        print(x + 1)
+    else:
+        print(x & -x)
+        
+
+def main():
+    multi = True  # Set to True for multi-test cases
+    t = inp() if multi else 1
+    for _ in range(t):
+        solve()
+
+if __name__ == '__main__':
+    main()
